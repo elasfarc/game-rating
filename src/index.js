a38287e9c1f64932539367535ab6d71d83c3b8e5
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-04 17:21:39 +0200

    Add styles for Adding a new comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/styles/main.css';
 import './assets/styles/item/container.css';
 import './assets/styles/item/details.css';
 import './assets/styles/item/comments.css';
+import './assets/styles/item/new-comment.css';
 import { displayPopup } from './item.js';
 
 import pic from './assets/imgs/2.png';
