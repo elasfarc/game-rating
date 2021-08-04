a2944cb5f6745fcd2b4d7d084f772bd5016254d7
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-04 06:00:19 +0200

    bypass CORS by using a DNS server

--- src/services/api/utilities/provider.js
+++ src/services/api/utilities/provider.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 
+const CORS_URL = 'https://cors-anywhere.herokuapp.com';
 const BASE_URL = 'https://age-of-empires-2-api.herokuapp.com/api/v1';
 
 export const get = async ({ entrypoint }) => {
-  const url = `${BASE_URL}/${entrypoint}`;
+  const url = `${CORS_URL}/${BASE_URL}/${entrypoint}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
