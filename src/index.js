8ae8a95d96c7d6b2be09a439aa7e8c9225b60674
Zibilyonik <onatsurmeli@gmail.com>
2021-08-05 16:01:38 +0200

    adjust mainpage styling and positioning'

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import './assets/styles/item/details.css';
 import './assets/styles/item/comments.css';
 import { displayPopup } from './item.js';
 import fillPage from './mainpage.js';
-import pic from './assets/imgs/2.png';
 
 const btn = document.querySelector('#btn');
-// btn.addEventListener('click', async () => {
-//   const result = await get({ entrypoint: 'civilization/1' });
-//   console.log(result);
-// });
 btn.addEventListener('click', async () => {
   await fillPage();
+  await displayPopup(1);
 });
--- src/mainpage.js
+++ src/mainpage.js
@@ -6,17 +6,6 @@ function importAll(r) {
 
 const images = importAll(require.context('./assets/img/', false, /\.(png|jpe?g|svg)$/));
 
-const createElement = (name = 'temp') => {
-  const base = document.getElementById('grid');
-  const elemContainer = document.createElement('div');
-  elemContainer.classList.add('col-4', 'list-element', 'mx-2');
-  const img = document.createElement('img');
-  img.src = `./assets/img/${name.toLowerCase()}.png`;
-  img.alt = '';
-  img.classList.add('civ-img-style', 'mx-auto');
-  elemContainer.appendChild(img);
-  base.appendChild(elemContainer);
-};
 const fillPage = async () => {
   document.getElementById('grid').innerHTML = '';
   const entrypoint = 'civilizations';
@@ -31,17 +20,26 @@ const fillPage = async () => {
   sorted.splice(9, 1);
   sorted.splice(15, 1);
   for (let i = 0; i < sorted.length; i += 1) {
-    const { name } = sorted[i];
-    console.log(`${name} and ${i}`);
     const base = document.getElementById('grid');
     const elemContainer = document.createElement('div');
-    elemContainer.classList.add('col-4', 'list-element');
+    elemContainer.classList.add('col-2', 'list-element', 'card');
+    elemContainer.style = 'width: 12vw';
     const img = document.createElement('img');
     img.src = images[i];
-    img.alt = '';
-    img.classList.add('civ-img-style', 'mx-auto');
+    img.alt = sorted[i].name;
+    img.classList.add('civ-img-style', 'card-img-top', 'mx-auto');
     elemContainer.appendChild(img);
-    if (i % 3 === 0) {
+    const body = document.createElement('div');
+    body.classList.add('civ-body-style', 'card-body');
+    const text = document.createElement('h5');
+    text.classList.add('civ-text-style', 'card-title');
+    const likeButton = document.createElement('a');
+    likeButton.classList.add('civ-like-button', 'btn', 'btn-primary');
+    likeButton.innerText = 'Like';
+    body.appendChild(text);
+    body.appendChild(likeButton);
+    elemContainer.appendChild(body);
+    if (i % 6 === 0) {
       const line = document.createElement('div');
       line.classList.add('w-100', 'my-3');
       base.appendChild(line);
@@ -49,5 +47,4 @@ const fillPage = async () => {
     base.appendChild(elemContainer);
   }
 };
-
 export default fillPage;
