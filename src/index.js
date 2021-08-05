843866004ee66a766ce6b9c602a8e4f5f250b3c9
Zibilyonik <onatsurmeli@gmail.com>
2021-08-05 02:19:16 +0200

    Create mainpage module -Create fillPage function to fill the page -Create createElement function to create elements. -Adjust icons to fit the API information

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/styles/item/container.css';
 import './assets/styles/item/details.css';
 import './assets/styles/item/comments.css';
 import { displayPopup } from './item.js';
+import fillPage from './mainpage.js';
 
 import pic from './assets/imgs/2.png';
 
@@ -14,6 +15,7 @@ const btn = document.querySelector('#btn');
 // });
 const itemID = 1;
 btn.addEventListener('click', async () => {
+  await fillPage();
   await displayPopup(itemID);
 });
 
---
+++ src/mainpage.js
@@ -0,0 +1,46 @@
+import { get } from './services/api/utilities/provider.js';
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const images = importAll(require.context('./assets/img/', false, /\.(png|jpe?g|svg)$/));
+
+const createElement = (name = 'temp') => {
+  const base = document.getElementById('grid');
+  const elemContainer = document.createElement('div');
+  elemContainer.classList.add('col-3', 'list-element', 'mx-2');
+  const img = document.createElement('img');
+  img.src = `./assets/img/${name.toLowerCase()}.png`;
+  img.alt = '';
+  img.classList.add('civ-img-style', 'mx-auto');
+  elemContainer.appendChild(img);
+  base.appendChild(elemContainer);
+};
+const fillPage = async () => {
+  document.getElementById('grid').innerHTML = '';
+  const entrypoint = 'civilizations';
+  const result = await get({ API: 'AOE', entrypoint });
+  const sorted = [...result.civilizations].sort((a, b) => a.name.localeCompare(b.name));
+  const uniqueList = sorted.filter((item, pos) => sorted.indexOf(item) === pos);
+  for (let i = 0; i < uniqueList.length; i += 1) {
+    const { name } = uniqueList[i];
+    console.log(`${name} and ${i}`);
+    const base = document.getElementById('grid');
+    const elemContainer = document.createElement('div');
+    elemContainer.classList.add('col-3', 'list-element', 'mx-2');
+    const img = document.createElement('img');
+    img.src = images[i];
+    img.alt = '';
+    img.classList.add('civ-img-style', 'mx-auto');
+    elemContainer.appendChild(img);
+    if (i % 3 === 0) {
+      const line = document.createElement('div');
+      line.classList.add('w-100');
+      base.appendChild(line);
+    }
+    base.appendChild(elemContainer);
+  }
+};
+
+export default fillPage;
