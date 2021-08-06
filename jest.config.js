00a83dccd0879386f5ff158b3f9e012847ecaa92
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-06 15:53:15 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ["/node_modules/(?!vue-awesome)"],
-  "moduleNameMapper": {
-    "\\.(jpg|ico|jpeg|png|gif)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js"
-  }
+  transformIgnorePatterns: ['/node_modules/(?!vue-awesome)'],
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
