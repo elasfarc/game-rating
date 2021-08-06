f2a1720306b8943f6a32e27c690615e8a02cb6c1
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-06 14:05:17 +0200

    Mock image files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: ["/node_modules/(?!vue-awesome)"],
+  "moduleNameMapper": {
+    "\\.(jpg|ico|jpeg|png|gif)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js"
+  }
 };
